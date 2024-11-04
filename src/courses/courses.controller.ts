784d9e1a04e79c833b41be85a4576713a0595081
danrleydimas <danrley-dimas@hotmail.com>
2024-11-04 14:35:07 -0300

    creater services and modules

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,26 +1,67 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Put, Res } from '@nestjs/common';
+import { CoursesService } from './courses.service';
+
 
 @Controller('courses')
 export class CoursesController {
-    @Get()
-    findAll(){
-        return 'Listagem de cursos'
+    //injetando service
+    constructor(private readonly courseService:CoursesService){
+
     }
 
+    @Get('express')
+    findAllExpress(@Res() response) {
+        return response.status(200).send('listagem de cursos') // utilizando o response do express
+    }
+
+    @Get()
+    findAll(){
+        return this.courseService.findAll()
+    }
+
+
     @Get(':id')
-    findOne(@Param('id') id: string ){
-        return `Curso com ID ${id}`
+    findOne(@Param('id') id: number) {
+        return this.courseService.findOne(+id)
     }
     // pegar dois parametros na rota
     @Get(':id/:name')
-    findOneByTwoParams(@Param() params){
+    findOneByTwoParams(@Param() params) {
         return `Curso com ID ${params.id} e com Name ${params.name}`
     }
     // descontruir parametros
 
     @Get(':id/:name/:idade')
-    findOneByThreeParams(@Param('id') id:string , @Param('name') name:string, @Param('idade') idade:string ){
+    findOneByThreeParams(@Param('id') id: string, @Param('name') name: string, @Param('idade') idade: string) {
         return `Curso com ID ${id} , Name ${name} e com Idade ${idade} `
     }
 
+    //trabalhando com method que trata dado pelo corpo da requisição
+    // alterando o status code das respotas
+    @HttpCode(204) //204 retorna nada que é bom para quando é uma rota para excluir
+    @Post()
+    create(@Body() body) {
+        return this.courseService.create(body);
+    }
+
+
+    //atualizar dados
+    //put para atualizar um conjuto de dados
+    //patch para atualizar uma propriedade
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        console.log(body)
+        return `Update course with ID ${id}`
+    }
+
+    @Put(':id')
+    updatePut (@Param ('id') id: number, @Body() body){
+        return this.courseService.update(+id,body)
+    }
+
+    @HttpCode(HttpStatus.NO_CONTENT) //204 retorna nada que é bom para quando é uma rota para excluir
+    @Delete(':id')
+    remove(@Param('id') id: number){
+        return this.courseService.remove(+id)
+    }
 }
